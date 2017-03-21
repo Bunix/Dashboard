47a4c5400a40b42140b9d2b31d0b010290cd0b37
Roc <roc.sun@fabricgroup.com.au>
2017-03-21 20:13:49 +0800

    Change page style.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,14 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>INSPINIA - @yield('title') </title>
 
-    <link rel="stylesheet" href="{{asset('css/vendor.css')}}"/>
-    <link rel="stylesheet" href="{{asset('css/app.css')}}"/>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <script>
-        window.Laravel = {!! json_encode([
-            'csrfToken' => csrf_token(),
-        ]) !!};
-    </script>
+
+    <link rel="stylesheet" href="{!! asset('css/vendor.css') !!}" />
+    <link rel="stylesheet" href="{!! asset('css/app.css') !!}" />
+
 </head>
 <body>
 
@@ -24,24 +20,26 @@
 
         <!-- Page wraper -->
         <div id="page-wrapper" class="gray-bg">
+
             <!-- Page wrapper -->
-            <div id="app">
             @include('layouts.topnavbar')
 
             <!-- Main view  -->
             @yield('content')
+
             <!-- Footer -->
             @include('layouts.footer')
-            </div>
+
         </div>
         <!-- End page wrapper-->
 
     </div>
     <!-- End wrapper-->
 
-
+<script src="{!! asset('js/app.js') !!}" type="text/javascript"></script>
 
 @section('scripts')
-@show<script src="{!! asset('js/app.js') !!}" type="text/javascript"></script>
+@show
+
 </body>
 </html>
